8d362361340c1bb4becf5ed6831c510530f95772
gumba <user@komp>
2014-11-26 18:50:24 +0100

    Fix klas Vector2D i Vector3D, dodanie testów

---
+++ test/vectors/VectorsSuite.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vectors;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author user
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({vectors.Vector3DTest.class, vectors.Vector2DTest.class})
+public class VectorsSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
